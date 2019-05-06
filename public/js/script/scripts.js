b50f0f5ce0344d4c88d1941b4c268e11758d4760
Erlis <erlisliiva@hot.ee>
2019-05-07 01:08:37 +0300

    initial Commit

---
+++ public/js/script/scripts.js
@@ -0,0 +1,134 @@
+$(function () {
+    let socket = io();
+    let users = [];
+    let typingTimeout;
+    let titleTimeout;
+    let clientUsername;
+
+    $("#chat-screen").hide();
+
+    $("#user-login").click(function (e) {
+        $("#username-empty").hide();
+        $("#username-taken").hide();
+
+        e.preventDefault(); // prevents page reloading
+        let username = $("#username").val().trim();
+        if (username.length < 1) {
+            $("#username-empty").show();
+            return false;
+        }
+
+        users = getUsers();
+        if (users.includes(username)) {
+            $("#username-taken").show();
+            return false;
+        }
+        socket.emit("user joined", username);
+        $("#login-screen").hide();
+        $("#chat-screen").show();
+        clientUsername = username;
+        return false;
+    });
+
+    $("#username").keyup(function (event) {
+        if (event.which === 13) {
+            $('#user-login').click(); // enter functionality
+        }
+    });
+
+    $("#message-send").click(function (e) {
+        e.preventDefault();// prevents page reloading
+        let $message = $('#message');
+        let messageText = $message.val().trim();
+        if (messageText.length < 1) return;
+        $message.val("");
+        socket.emit("message", messageText);
+        socket.emit('user typing', false); // remove 'typing...' after message sent
+    });
+
+    socket.on('user joined', function (username) {
+        renderUserTable();
+    });
+
+    socket.on('user left', function (username) {
+        renderUserTable();
+    });
+
+    socket.on('message', function (messageData) {
+        let user = messageData.user;
+        let text = messageData.parsedText;
+        let currentDate = new Date().toLocaleTimeString();
+
+        createMessageBubble(user, text, currentDate);
+
+        let tableRow = $('<tr>')
+            .append($('<td>').append(`${user}`))
+            .append($('<td>').append(`${text}`))
+            .append($('<td>').append(`${currentDate}`));
+        $('#message-table').append(tableRow);
+
+    });
+
+    $('#message').keyup(function (event) {
+        socket.emit('user typing', true);
+
+        if (event.which === 13 && !event.shiftKey) {
+            $('#message-send').click(); // send on enter, new line with shift + enter
+        }
+    });
+
+    socket.on('user typing', function (data) {
+        if (data.typing) {
+            $(`#${data.username}`).show()
+        } else {
+            $(`#${data.username}`).hide()
+        }
+    });
+
+    function getUsers() {
+        let res = [];
+        $.ajax({
+            url: '/users',
+            success: function (response) {
+                if (response.length === 0) return res;
+                for (let i = 0; i < response.length; i++) {
+                    res.push(response[i]);
+                }
+            },
+            async: false
+        });
+        return res;
+    }
+
+    function renderUserTable() {
+        let userTable = $("#user-table");
+        userTable.empty();
+        users = getUsers();
+        users.forEach(function (value, index) {
+            let userRow = $('<div>')
+                .append($('<i>').addClass("far fa-user"))
+                .append($('<span>').append(value).addClass('user-table-text'))
+                .append($('<span>').append('<i>kirjutab...</i>').attr('id', value).addClass('is-typing-label'));
+            userTable.append(userRow);
+        })
+    }
+
+    function createMessageBubble(user, text, time) {
+        let $messageContainer = $('.message-container');
+
+        let messageDiv = $('<div>');
+        let userLabel = $('<span>').append(`<strong>${user}</strong> <i style="font-size: 12px">${time}</i>`);
+        let textContainer = $('<div>').append(`<p>${text}</p>`);
+
+        messageDiv.addClass('message-bubble');
+        userLabel.addClass('message-sender');
+        textContainer.addClass('message-text');
+        if (user === clientUsername) messageDiv.addClass('client-message');
+
+        messageDiv.append(userLabel);
+        messageDiv.append(textContainer);
+
+        $messageContainer.append(messageDiv);
+        $messageContainer.scrollTop($messageContainer.prop("scrollHeight"));
+    }
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let port = 3000;
+let users = [];
+
+
+let md = require('markdown-it')({
+    linkify: true
+});
+
+app.use('/css', express.static(__dirname + '/node_modules/bulma/css/'));
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/users', function (req, res) {
+    res.send(users);
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('user joined', function (username) {
+        socket.username = username;
+        console.log(`${socket.username} joined!`);
+        users.push(socket.username);
+        io.emit('user joined', socket.username);
+    });
+
+    socket.on('disconnect', function () {
+        users.splice(users.indexOf(socket.username), 1);
+        io.emit('user left', socket.username);
+        console.log(`${socket.username} disconnected!`);
+    });
+
+    socket.on('message', function (msg) {
+        console.log('message: ' + msg);
+        let mdText = md.render(msg);
+        io.emit('message', {user: socket.username, parsedText: mdText, plaintext: msg});
+    });
+
+    socket.on('user typing', function (isTyping) {
+        io.emit('user typing', { username: socket.username, typing: isTyping});
+    })
+});
+
+http.listen(process.env.PORT || port, function () {
+    console.log(`Server started at localhost:${port}`)
+});
